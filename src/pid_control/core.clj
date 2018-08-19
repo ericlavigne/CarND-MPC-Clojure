6d444a3f52148c909496a7f0e1264cbbbbfda348
Eric Lavigne <lavigne.eric@gmail.com>
2018-08-19 02:34:53 -0400

    Recognize and respond to empty telemetry message
    The simulator sends an empty telemetry message to indicate that
    the car is being driven manually. Changed the client to respond
    to such messages with the expected acknowledgement that the
    car is being driven manually.

--- src/pid_control/core.clj
+++ src/pid_control/core.clj
@@ -6,7 +6,9 @@
             [clojure.string :refer [index-of last-index-of]])
   (:gen-class))
 
-(defn parse-telemetry [msg]
+(defn parse-message
+  "Parse message from Udacity's SDC term 2 simulator for the PID project."
+  [msg]
   (if (and msg
            (> (.length msg) 2)
            (= (subs msg 0 2) "42"))
@@ -16,31 +18,34 @@
           json-msg (json/read-str json-str)]
       (if (= (get json-msg 0) "telemetry")
         (let [data (get json-msg 1)]
-          {:cte (Double/parseDouble (get data "cte"))
-           :steering-angle (Double/parseDouble (get data "steering_angle"))
-           :throttle (Double/parseDouble (get data "throttle"))
-           :speed (Double/parseDouble (get data "speed"))
-           ;:image (get data "image")
-           })
+          (if data
+            {:type :telemetry
+             :cte (Double/parseDouble (get data "cte"))
+             :steering-angle (Double/parseDouble (get data "steering_angle"))
+             :throttle (Double/parseDouble (get data "throttle"))
+             :speed (Double/parseDouble (get data "speed"))
+             ;:image (get data "image")
+            }
+            {:type :manual}))
         json-msg))
     nil))
 
-(def delay-millis 100)
-
-(defn handler [{:keys [ws-channel] :as req}]
+(defn handler
+  "Called in response to websocket connection. Handles sending and receiving messages."
+  [{:keys [ws-channel] :as req}]
   (go-loop []
     (let [{:keys [message]} (<! ws-channel)
-          telemetry (parse-telemetry message)]
+          parsed (parse-message message)]
       (if message
-        (println (str (or telemetry message))))
-      (if telemetry
-        (let [response "42[\"steer\",{\"steering_angle\":0.0,\"throttle\":0.3}]"]
-          (>! ws-channel response)
-          (println response)))
-      ;(>! ws-channel "42[\"manual\",{}]")
-      (Thread/sleep delay-millis)
-      (recur)
-      )))
+        (println (str (or parsed message))))
+      (when parsed
+        (let [response (case (:type parsed)
+                         :telemetry "42[\"steer\",{\"steering_angle\":0.0,\"throttle\":0.3}]"
+                         :manual    "42[\"manual\",{}]"
+                         (throw (Exception. (str "Unrecognized message type " (:type parsed)))))]
+            (>! ws-channel response)
+            (println response)))
+      (recur))))
 
 (defn -main
   "Run websocket server to communicate with Udacity PID simulator."
@@ -50,5 +55,4 @@
                   (wrap-websocket-handler
                     {:read-ch (chan (dropping-buffer 10))
                      :format :str}))
-              {:port 4567})
-  )
+              {:port 4567}))
--- test/pid_control/core_test.clj
+++ test/pid_control/core_test.clj
@@ -5,11 +5,14 @@
 
 (deftest parse-telemetry-test
   (testing "Can parse a telemetry message"
-    (let [parsed (parse-telemetry (slurp (io/resource "telemetry-message.txt")))]
+    (let [parsed (parse-message (slurp (io/resource "telemetry-message.txt")))]
       (is (= 0.7598 (:cte parsed)))
       (is (= 0.2 (:steering-angle parsed)))
       (is (= 0.5 (:throttle parsed)))
       (is (= 10.3 (:speed parsed)))))
   (testing "Can skip empty messages"
-    (is (= nil (parse-telemetry nil)))
-    (is (= nil (parse-telemetry "2")))))
+    (is (= nil (parse-message nil)))
+    (is (= nil (parse-message "2"))))
+  (testing "Can parse manual message"
+    (is (= {:type :manual}
+           (parse-message "42[\"telemetry\",null]")))))
